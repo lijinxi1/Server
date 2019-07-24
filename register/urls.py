fa9889e3b632e8d1eca7e3bc5d33e218e95eb7fb
李金熹 <32548701+TXLJX@users.noreply.github.com>
2019-07-24 20:31:27 +0800

    register模块

--- register/urls.py
+++ register/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from .views import register
+from .views import register,register_result
 
 
 urlpatterns=[
-    re_path('^register',register,name='注册')
+    re_path('^register/$',register,name='注册'),
+    re_path('^register_result',register_result,name='注册提交')
 ]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,65 @@
 from django.shortcuts import render
-
+from django.core.exceptions import *
 # Create your views here.
-
+from .models import Teacher,Student
 def register(requests):
     return render(requests,'register/register.html')
+
+
+def register_result(requests):
+    identity=requests.POST.get('identity')
+    _id=requests.POST.get('_id')
+    name=requests.POST.get('name')
+    class_office=requests.POST.get('class_office')
+    email=requests.POST.get('email')
+    phone=requests.POST.get('phone')
+    addr=requests.POST.get('address')
+    print(identity)
+    if identity=='teacher':
+        if _id!='':
+            try:
+                t=Teacher.objects.get(teacher_id=_id)
+            except  ObjectDoesNotExist:
+                text="教师已成功注册"
+                t = Teacher(teacher_id=_id, teacher_name=name, teacher_office=class_office,
+                            teacher_email=email, teacher_phone=phone, teacher_addr=addr)
+                t.save()
+            else:
+                t.teacher_id=_id
+                t.teacher_email=email
+                t.teacher_office=class_office
+                t.teacher_name=name
+                t.teacher_phone=phone
+                t.teacher_addr=addr
+                t.save()
+                text="教师信息已更新"
+
+    else:
+        if _id!='':
+            try:
+                s=Student.objects.get(stu_id=_id)
+            except ObjectDoesNotExist:
+                s=Student(stu_id=_id,stu_name=name,stu_class=class_office,
+                      stu_email=email,stu_phone=phone,stu_addr=addr)
+                s.save()
+                text="学生已成功注册"
+            else:
+                s.stu_id=_id
+                s.stu_class=class_office
+                s.stu_addr=addr
+                s.stu_phone=phone
+                s.stu_email=email
+                s.stu_name=name
+                s.save()
+                text='学生信息已更新'
+
+    context={}
+    context['text']=text
+    context['st_id']=_id
+    context['name']=name
+    context['class_office']=class_office
+    context['email']=email
+    context['phone']=phone
+    context['addr']=addr
+    context['identity']=identity
+    return render(requests,'register/register_result.html',context=context)
